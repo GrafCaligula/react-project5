58a8516e5d57582fba17dc81f068746ad7c394d8
Markus <markus.kloepper@gmx.net>
2022-11-07 11:23:35 +0100

    implemented header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,16 @@
 
 export default function Header() {
   return (
-    <h1>Works!</h1>
+    <div className="header flex">
+      <div className="header-logo flex">
+        <img 
+          src={require("./images/trollFace.png")}
+          alt="logo"
+          className="logo"
+        />
+        <h1>Meme Generator</h1>
+      </div>
+      <div className="header-title">React Course - Project 3</div>
+    </div>
   )
 }
